fbfcb69f2a094effd8865039c010d598df1476bb
Ignacio Serrano Zabaleta <ignacio.serrano@gft.com>
2017-04-15 20:47:49 +0200

    Added ETag.

--- src/main/java/com/gft/isz/amdc/Application.java
+++ src/main/java/com/gft/isz/amdc/Application.java
@@ -2,9 +2,12 @@ package com.gft.isz.amdc;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 public class Application {
@@ -15,5 +18,11 @@ public class Application {
     @Bean
     public ExecutorService executorService() {
        return Executors.newFixedThreadPool(10);
+    }
+    
+    /* Adds ETag cache control for GET operations. */
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+      return new ShallowEtagHeaderFilter();
     }
 }
